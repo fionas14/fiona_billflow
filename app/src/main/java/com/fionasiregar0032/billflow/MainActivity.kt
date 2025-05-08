21b34aeebfe349f07528b5cf0f926f55b653a337
fionas14 <siregarfiona90@gamil.com>
2025-05-09 04:19:42 +0700

    Menambahkan TopApp BAr

--- app/src/main/java/com/fionasiregar0032/billflow/MainActivity.kt
+++ app/src/main/java/com/fionasiregar0032/billflow/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.fionasiregar0032.billflow.ui.theme.BillflowTheme
+
+import android.content.res.Configuration
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +26,46 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BillflowTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
             }
+            MainScreen()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainScreen() {
+
+    Scaffold (
+        containerColor = Color(0xFFFFF5E1),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Bill Flow")
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = Color(0xFF800000),
+                    titleContentColor = Color.White,
+                )
+            )
+        }
+    ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+    } }
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "",
         modifier = modifier
     )
 }
 
+
 @Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 fun GreetingPreview() {
     BillflowTheme {
-        Greeting("Android")
+
     }
 }
